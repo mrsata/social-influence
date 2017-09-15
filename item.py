91cc2cf4d156412f4b3991757ecdfec18f3a1ffc
mrsata <liuhanrick@gmail.com>
2017-09-15 15:22:12 -0500

    Init

---
+++ item.py
@@ -0,0 +1,6 @@
+class Item(object):
+    """Abstract base class for items
+
+    """
+
+    def __init__(self):
---
+++ user.py
@@ -0,0 +1,6 @@
+class User(object):
+    """Abstract base class for users
+
+    """
+
+    def __init__(self):
