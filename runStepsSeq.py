1b08d2808b77cdc7e638fd8063bc088ad387eaac
mrsata <liuhanrick@gmail.com>
2017-11-20 03:19:31 -0600

    Update params; Add argparser

--- runStepsSeq.py
+++ runStepsSeq.py
@@ -1,48 +1,58 @@
 from __future__ import division
 from copy import deepcopy
-from itertools import repeat
-from optparse import OptionParser
 import random
-# import matplotlib.pyplot as plt
-import multiprocessing as mp
 import numpy as np
 import scipy.stats as stats
+import argparse
 import time
-
+import os
 from item import Item
 from user import User
 from plat2d import Platform
 
-parser = OptionParser()
-parser.add_option("-t", type="float", default=0.0, dest="tau")
-parser.add_option("-c", type="float", default=0.0, dest="c")
-options = parser.parse_args()[0]
-t, c = options.tau, options.c
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--tau', nargs='*', type=float)
+parser.add_argument('-u', '--uc', nargs='*', type=float)
+parser.add_argument('-n', '--nshow', nargs='*', type=float)
+parser.add_argument('-l', '--lc', nargs='*', type=float)
+args = parser.parse_args()
 
 rdm_quality = False  # assign item quality randomly
-plotQuality = False  # plot item quality
 num_free = 1  # number of free views upon initialization
-num_runs = 10  # number of realizations
+num_runs = 1  # number of realizations
 num_item = 50  # total number of items
-num_user = 100000  # total number of users (time)
+num_user = 500000  # total number of users (time)
 lower, upper = 0, 1  # lower and upper bound of item quality
 mu, sigma = 0.5, 0.3  # mean and standard deviation of item quality
 ability_range = range(1, 6)  # ability of 1~5
-rankModes = ['quality', 'ucb']
-viewModes = ['first', 'position']
-viewMode = viewModes[1]  # how platform displays items to users
-n_showed = 1  # number of items displayed by the platform
 p_pos = 0.5  # ratio of positional preference in user's choice
-# p_pos=1 has only positional prefernece
-user_c = 0.5  # coeff of user's lcb
-tau_and_cs = np.mgrid[t:t+0.5:.1, c:c+3:.1]
-tau_and_cs = np.concatenate(
-    (tau_and_cs[0][:, :, np.newaxis], tau_and_cs[1][:, :, np.newaxis]),
-    axis=2).reshape(-1, 2)
-tol_cnvrg = 0.005
-tol_close = 0.005
+
+## PARAMETERS
+parameters = ['tau', 'uc', 'nshow', 'lc']
+fixed_tau, fixed_uc, fixed_nshow, fixed_lc = 1, 1, 1, 1
+paras = [args.tau, args.uc, args.nshow, args.lc]
+paras = [parameters[i] for i, para in enumerate(paras) if para != None]
+para_ranges = []
+for i in range(2):
+    if paras[i] == 'tau':
+        taus = args.tau if len(args.tau) == 3 else args.tau + [.25]
+        para_ranges.append(np.arange(*tuple(taus)))
+    elif paras[i] == 'uc':
+        ucs = args.uc if len(args.uc) == 3 else args.uc + [.25]
+        para_ranges.append(np.arange(*tuple(ucs)))
+    elif paras[i] == 'nshow':
+        nshows = args.nshow if len(args.nshow) == 3 else args.nshow + [.05]
+        para_ranges.append(np.arange(*tuple(nshows)))
+    elif paras[i] == 'lc':
+        lcs = args.lc if len(args.lc) == 3 else args.lc + [.25]
+        para_ranges.append(np.arange(*tuple(lcs)))
+
+mesh1, mesh2 = np.meshgrid(para_ranges[0], para_ranges[1])
+paras_grid = np.dstack((mesh1, mesh2)).reshape(-1, 2)
+
+# convergence condition
+tol_cnvrg = 1e-5
 num_consec = 1000
-#tau_and_cs = np.array(([1, .5], ))
 
 
 #********** Initilization
@@ -58,12 +68,6 @@ def initialize(seed):
         a, b = (lower - mu) / sigma, (upper - mu) / sigma
         qualities = stats.truncnorm(
             a, b, loc=mu, scale=sigma).rvs(size=num_item)
-        if plotQuality:
-            fig_idx += 1
-            plt.figure(fig_idx)
-            plt.hist(qualities, normed=True)
-            plt.title("Distribution of Item Quality")
-            plt.show()
 
     user0 = User(0, ability_range[-1])
     # assign qualities to items
@@ -89,23 +93,34 @@ def initialize(seed):
 
 
 #********** Simulation
-def simulate(inputs):
+def simulate(its, urs, paras, para_i):
     # np.random.seed(123)
-    items, users, tau_and_c = inputs
-    tau, c = tau_and_c
-    platform1 = Platform(items=deepcopy(items), users=users)
-    platform2 = Platform(items=deepcopy(items), users=users)
+    items = its
+    users = urs
+    tau, c, n_showed, user_c = fixed_tau, fixed_uc, fixed_nshow, fixed_lc
+    if paras == ['tau', 'uc']:
+        tau, c = para_i
+    elif paras == ['uc', 'nshow']:
+        c, n_showed = para_i
+    elif paras == ['nshow', 'lc']:
+        n_showed, user_c = para_i
+    elif paras == ['tau', 'nshow']:
+        tau, n_showed = para_i
+    elif paras == ['tau', 'lc']:
+        tau, user_c = para_i
+    elif paras == ['uc', 'lc']:
+        c, user_c = para_i
+    else:
+        tau, c, n_showed, user_c = fixed_tau, fixed_uc, fixed_nshow, fixed_lc
+    platform = Platform(items=deepcopy(items), users=users)
     happys1 = np.zeros(num_consec)
-    happys2 = np.zeros(num_consec)
     step = 0
     while step < num_user:
-        # if step % 500 == 0:
-        # print(step)
         happys0 = happys1
-        happy1 = platform1.step(
+        happy = platform.step(
             uid=step,
-            rankMode=rankModes[0],
-            viewMode=viewMode,
+            rankMode='ucb',
+            viewMode='position',
             evalMethod="upvote_only",
             numFree=num_free,
             n_showed=n_showed,
@@ -113,71 +128,69 @@ def simulate(inputs):
             user_c=user_c,
             tau=tau,
             c=c)
-        happy2 = platform2.step(
-            uid=step,
-            rankMode=rankModes[1],
-            viewMode=viewMode,
-            evalMethod="upvote_only",
-            numFree=num_free,
-            n_showed=n_showed,
-            p_pos=p_pos,
-            user_c=user_c,
-            tau=tau,
-            c=c)
-        happys1[step % num_consec] = happy1
-        happys2[step % num_consec] = happy2
+        happys1[step % num_consec] = happy
         converge = np.all(
             np.abs(happys1 - happys0[np.arange(num_consec) - 1]) < tol_cnvrg)
-        close = np.all(np.abs(happy1 - happy2) < tol_close)
-        if converge and close:
+        if converge:
             # quality converges and difference < tolerance
-            print(tau, c, step, happy1, happy2)
-            return tau, c, step, happy1, happy2
+            print(para_i[0], para_i[1], step, happy)
+            return para_i[0], para_i[1], step, happy
         step += 1
-    print(tau, c, step, happy1, happy2)
-    return tau, c, step, happy1, happy2
+    print(para_i[0], para_i[1], step, happy)
+    return para_i[0], para_i[1], step, happy
 
 
 #********** Start
-# pool = mp.Pool()
 t0 = time.time()
 print("-----Start\nnum_runs: {0}\nnum_item: {1}\nnum_user: {2}".format(
     num_runs, num_item, num_user))
 
 # Initialize
 seeds = range(num_runs)
-# inits = pool.map(initialize, seeds)
-inits = list(map(initialize, seeds))
-items, users = zip(*inits)
+items = []
+users = []
 for i in range(len(seeds)):
-    qualities = [itm.getQuality() for itm in items[i].values()]
-    mean_quality = np.mean(qualities)
-    # print("Seed: {:2}   mean: {}".format(seeds[i], mean_quality))
+    itms, usrs = initialize(seeds[i])
+    items.append(itms)
+    users.append(usrs)
 
 t_ini = time.time()
 print("-----Initialization takes %.4fs" % (t_ini - t0))
-print("tau", "c", "step", rankModes[0], rankModes[1])
+print(paras[0], paras[1], "step", 'ucb')
 
 # Simulate
 results = []
-for i in range(len(tau_and_cs)):
-    # result = pool.map_async(simulate, zip(items, users, repeat(tau_and_cs[i])))
-    result = list(map(simulate, zip(items, users, repeat(tau_and_cs[i]))))
-    results.append(result)
+m, n = paras_grid.shape
+for i_run in range(num_runs):
+    itms = items[i_run]
+    usrs = users[i_run]
+    result_realization = []
+    for i in range(m):
+        result = simulate(itms, usrs, paras, paras_grid[i])
+        result_realization.append(result)
+    results.append(result_realization)
 
-# Output
-for result in results:
-    # happys = result.get()
-    happys = result
-    tau, c = happys[0][0], happys[0][1]
-    with open('outputs/output_tau{0}_c{1}'.format(tau, c), 'w') as f:
-        for happy in happys:
-            f.write(', '.join(map(str, happy)) + '\n')
-    happys = np.array(happys)
-    with open('outputs/output_all_mean', 'a') as f:
-        f.write(', '.join(map(str, np.mean(happys, axis=0))) + '\n')
-    with open('outputs/output_all_median', 'a') as f:
-        f.write(', '.join(map(str, np.median(happys, axis=0))) + '\n')
+### Output
+results = np.array(results)
+outputpath = 'output'
+if not os.path.isdir(outputpath):
+    os.makedirs(outputpath)
+savepath = outputpath + '/' + paras[0] + '&' + paras[1]
+if not os.path.isdir(savepath):
+    os.makedirs(savepath)
+for i in range(m):
+    p1, p2 = paras_grid[i]
+    with open(
+        (savepath + '/' + paras[0] + '{0}_' + paras[1] + '{1}.txt').format(
+            p1, p2), 'w') as f:
+        f.write(paras[0] + " " + str(p1) + "  " + paras[1] + " " + str(p2) +
+                "  number of realizations " + str(num_runs) + "\n")
+        f.write("   #step    happiness(quality)    happiness(ucb) \n")
+        f.write('\n'.join(map(str, results[:, i, 2:])) + '\n')
+        f.write("mean  " + str(np.mean(results[:, i, 2:], axis=0)) + "\n")
+        f.write("median" + str(np.median(results[:, i, 2:], axis=0)) + "\n")
+        f.write("max   " + str(np.max(results[:, i, 2:], axis=0)) + "\n")
+        f.write("min   " + str(np.min(results[:, i, 2:], axis=0)) + "\n")
 
 t_done = time.time()
 print("-----Simulation takes %.4fs" % (t_done - t_ini))
