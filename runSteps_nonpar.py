a821ceb479e34d6477eb60f370745ba992e8c83e
xr-cc <xr-cc@users.noreply.github.com>
2017-11-19 19:12:14 -0600

    add lcb_c, modify simulate

--- runSteps_nonpar.py
+++ runSteps_nonpar.py
@@ -8,38 +8,51 @@ import time
 from item import Item
 from user import User
 from plat2d import Platform
+import os
 
 rdm_quality = False  # assign item quality randomly
-plotQuality = False  # plot item quality
+#plotQuality = False  # plot item quality
 num_free = 1  # number of free views upon initialization
 num_runs = 10  # number of realizations
 num_item = 50  # total number of items
-num_user = 100000  # total number of users (time)
+num_user = 500000  # total number of users (time)
 lower, upper = 0, 1  # lower and upper bound of item quality
 mu, sigma = 0.5, 0.3  # mean and standard deviation of item quality
 ability_range = range(1, 6)  # ability of 1~5
 rankModes = ['quality', 'ucb']
 viewModes = ['first', 'position']
 viewMode = viewModes[1]  # how platform displays items to users
-n_showed = 0.1  # portion of items displayed by the platform
 p_pos = 1  # ratio of positional preference in user's choice
 # p_pos=1 has only positional prefernece
-user_c = 0.5  # coeff of user's lcb
-parameters = ['tau','c','n_showed']
-paras = [parameters[1],parameters[2]]
+
+## PARAMETERS
+parameters = ['tau','uc','nshow','lc']
+# fixed values
+fixed_tau, fixed_uc, fixed_nshow, fixed_lc = 1, 1, 1, 1 
+# PARAMETERS CHANGED: need to be in order
+paras = [parameters[1],parameters[2]] 
 para_ranges = []
 for i in range(2):
     if paras[i]=='tau':
-        para_ranges.append(np.linspace(0,2,3))
-    elif paras[i]=='c':
-        para_ranges.append(np.linspace(0,2,9))
-    elif paras[i]=='n_showed':
-        para_ranges.append(np.linspace(0.1,1,10))
+#        para_ranges.append(np.linspace(0,2,3))
+        para_ranges.append(np.linspace(0,5,21))
+    elif paras[i]=='uc':
+#        para_ranges.append(np.linspace(0,2,5))
+        para_ranges.append(np.linspace(0,3,13))
+    elif paras[i]=='nshow':
+#        para_ranges.append(np.linspace(0.1,1,4))
+        para_ranges.append(np.linspace(0.1,1,19))
+    elif paras[i]=='lc':
+#        para_ranges.append(np.linspace(0,2,5))
+        para_ranges.append(np.linspace(0,3,13))
+        
 mesh1,mesh2 = np.meshgrid(para_ranges[0], para_ranges[1])
 paras_grid = np.dstack(
     (mesh1,mesh2)).reshape(-1, 2)
-tol_cnvrg = 0.005
-tol_close = 0.005
+
+# convergence condition
+tol_cnvrg = 0.0001
+tol_close = 0.0001
 num_consec = 1000
 
 
@@ -56,12 +69,6 @@ def initialize(seed):
         a, b = (lower - mu) / sigma, (upper - mu) / sigma
         qualities = stats.truncnorm(
             a, b, loc=mu, scale=sigma).rvs(size=num_item)
-        if plotQuality:
-            fig_idx += 1
-            plt.figure(fig_idx)
-            plt.hist(qualities, normed=True)
-            plt.title("Distribution of Item Quality")
-            plt.show()
 
     user0 = User(0, ability_range[-1])
     # assign qualities to items
@@ -91,14 +98,26 @@ def simulate(its, urs, paras, para_i):
     # np.random.seed(123)
     items = its
     users = urs
-    if ('tau' in paras) and ('c' in paras):
+    tau, c, n_showed, user_c = fixed_tau, fixed_uc, fixed_nshow, fixed_lc
+    if paras==['tau','uc']:
+#    if ('tau' in paras) and ('uc' in paras):
         tau, c = para_i
-        n_showed = 1
-    elif ('c' in paras) and ('n_showed' in paras):
+#    elif ('uc' in paras) and ('nshow' in paras):
+    elif paras==['uc','nshow']:
         c, n_showed = para_i
-        tau = 1
+    elif paras==['nshow','lc']:
+#    elif ('nshow' in paras) and ('lc' in paras):
+        n_showed, user_c = para_i
+    elif paras==['tau','nshow']:
+#    elif ('tau' in paras) and ('nshow' in paras):
+        tau, n_showed = para_i
+    elif paras==['tau','lc']:
+#    elif ('tau' in paras) and ('lc' in paras):
+        tau, user_c = para_i
+    elif paras==['uc','lc']:
+        c, user_c = para_i
     else:
-        tau, c, n_showed = 1, 1, 1
+        tau, c, n_showed, user_c = fixed_tau, fixed_uc, fixed_nshow, fixed_lc
     platform1 = Platform(items=deepcopy(items), users=users)
     platform2 = Platform(items=deepcopy(items), users=users)
     happys1 = np.zeros(num_consec)
@@ -132,7 +151,7 @@ def simulate(its, urs, paras, para_i):
         happys2[step % num_consec] = happy2
         converge = np.all(
             np.abs(happys1 - happys0[np.arange(num_consec) - 1]) < tol_cnvrg)
-        close = np.all(np.abs(happy1 - happy2) < tol_close)
+        close = np.all(np.abs(happys1 - happys2) < tol_close)
         if converge and close:
             # quality converges and difference < tolerance
             print(para_i[0], para_i[1], step, happy1, happy2)
@@ -180,9 +199,12 @@ for i_run in range(num_runs):
     
 ### Output
 results = np.array(results)
+savepath = 'outputs/'+paras[0]+'&'+paras[1]
+if not os.path.isdir(savepath):
+   os.makedirs(savepath)
 for i in range(m):
     p1,p2 = paras_grid[i]
-    with open(('outputs/output_'+paras[0]+'{0}_'+paras[1]+'{1}.txt').format(p1, p2), 'w') as f:
+    with open((savepath+'/'+paras[0]+'{0}_'+paras[1]+'{1}.txt').format(p1, p2), 'w') as f:
         f.write(paras[0]+" "+str(p1)+"  "+paras[1]+" "+str(p2)+"  number of realizations "+str(num_runs)+"\n")
         f.write("   #step    happiness(quality)    happiness(ucb) \n")
         f.write('\n'.join(map(str, results[:,i,2:])) + '\n')
